72d0b86de0171fe4d44dd683d33aeeed67b233fe
Urvashi Biswal <urvashibiswal326@gmail.com>
2018-06-10 12:02:09 -0400

    Added a problem to find balanced paranthesis

--- lib/algos/string_problems.py
+++ lib/algos/string_problems.py
@@ -1,3 +1,4 @@
+# Split  string into possible substrings from a dictionary
 def split_string(input_str, my_dict):
     num_ways = 0
     if input_str in my_dict:
@@ -23,3 +24,35 @@ def split_string_memoized(input_str, my_dict, memo):
             num_ways = num_ways + split_string_memoized(input_str[i+1:], my_dict, memo)
     memo[input_str] = num_ways
     return num_ways
+
+
+# Check for balanced paratheses in an expresssion; it should exmamine whether the paors ad orders are correct
+# https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-an-expression/
+def check_balanced_parantheses(input_str):
+    brace1 = 0
+    brace2 = 0
+    brace3 = 0
+
+    for i in range(0, len(input_str)):
+        if input_str[i] == "{":
+            brace1 = brace1+1
+        elif input_str[i] == "[":
+            brace2 = brace2+1
+        elif input_str[i] == "(":
+            brace3 = brace3+1
+
+        if input_str[i] == "}":
+            brace1 = brace1 - 1
+        elif input_str[i] == "]":
+            brace2 = brace2 - 1
+        elif input_str[i] == ")":
+            brace3 = brace3 - 1
+        if brace1 < 0:
+            return False
+        if brace2 < 0:
+            return False
+        if brace3 < 0:
+            return False
+
+    return brace1 == 0 and brace2 == 0 and brace3 == 0
+
--- tst/algos/test_string_problems.py
+++ tst/algos/test_string_problems.py
@@ -4,7 +4,7 @@ import unittest
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
 
-from lib.algos.string_problems import split_string, split_string_memoized
+from lib.algos.string_problems import split_string, split_string_memoized, check_balanced_parantheses
 
 
 class TestStringProblems(unittest.TestCase):
@@ -15,3 +15,11 @@ class TestStringProblems(unittest.TestCase):
     def test_split_string_memoized(self):
         num_ways = split_string_memoized("peachpie",{"pea", "ch", "pie", "peach"}, {})
         self.assertEquals(2, num_ways)
+
+    def test_check_balanced_parantheses(self):
+        input_str = "[project]{}[[]])("
+        self.assertFalse(check_balanced_parantheses(input_str))
+        input_str = ""
+        self.assertTrue(check_balanced_parantheses(input_str))
+        input_str = "(tree)[]{}(((())))"
+        self.assertTrue(check_balanced_parantheses(input_str))
