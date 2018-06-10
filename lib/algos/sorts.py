1384df06055781cd7dc71909819728a63f598cbb
Urvashi Biswal <urvashibiswal326@gmail.com>
2018-06-10 14:12:05 -0400

    Merge sort.

--- lib/algos/sorts.py
+++ lib/algos/sorts.py
@@ -1,22 +1,39 @@
 def bubble_sort(input_arr):
     for i in range(1, len(input_arr)):
         for j in range(i, 0, -1):
-            if input_arr[j] < input_arr[j-1]:
+            if input_arr[j] < input_arr[j - 1]:
                 # Exchange
                 temp = input_arr[j]
-                input_arr[j] = input_arr[j-1]
-                input_arr[j-1] = temp
+                input_arr[j] = input_arr[j - 1]
+                input_arr[j - 1] = temp
             else:
                 break
 
 
-def merge_sort(input_arr):
-    for i in range(1, len(input_arr)):
-        for j in range(i, 0, -1):
-            if input_arr[j] < input_arr[j-1]:
-                # Exchange
-                temp = input_arr[j]
-                input_arr[j] = input_arr[j-1]
-                input_arr[j-1] = temp
-            else:
-                break
+def merge_sort(arr, start, end):
+    if end-start == 1:
+        return
+    mid = (start + end) // 2
+    merge_sort(arr, start, mid)
+    merge_sort(arr, mid, end)
+    merge(arr, start, end)
+
+
+def merge(arr, start, end):
+    middle = (start + end) // 2
+    new_arr = []
+    i = start
+    j = middle
+    while i < middle and j < end:
+        if arr[i] < arr[j]:
+            new_arr.append(arr[i])
+            i = i+1
+        else:
+            new_arr.append(arr[j])
+            j = j + 1
+    if i < middle:
+        new_arr.extend(arr[i:middle])
+
+    if j < end:
+        new_arr.extend(arr[j:end])
+    arr[start:end] = new_arr[:]
--- tst/algos/test_sorts.py
+++ tst/algos/test_sorts.py
@@ -1,7 +1,7 @@
 import unittest
 
 from lib.algos.sorts import bubble_sort
-from lib.algos.sorts import merge_sort
+from lib.algos.sorts import merge_sort, merge
 
 
 class TestSorts (unittest.TestCase):
@@ -15,8 +15,14 @@ class TestSorts (unittest.TestCase):
         bubble_sort(input_arr)
         self.assertEquals([1, 1, 2, 2, 3, 5, 7, 9], input_arr)
 
+    def test_merge(self):
+        arr = [1, 3, 5, 7, 2, 4, 6, 8]
+        merge(arr, 0, len(arr))
+        self.assertEquals([1, 2, 3, 4, 5, 6, 7, 8], arr)
+
     def test_merge_sort(self):
         input_arr = [1, 3, 7, 2, 9, 5]
-        merge_sort(input_arr)
+        merge_sort(input_arr,0,len(input_arr))
         self.assertEquals([1, 2, 3, 5, 7, 9], input_arr)
+
 
